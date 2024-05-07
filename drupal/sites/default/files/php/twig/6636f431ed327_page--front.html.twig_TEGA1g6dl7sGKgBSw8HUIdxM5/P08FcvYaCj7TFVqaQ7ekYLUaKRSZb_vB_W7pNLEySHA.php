e8a5c253fe2e00e39b2a80d28c9ee6d4f8bc0607
a-Aram-a <aramtashchan@gmail.com>
2024-05-07 08:37:29 +0300

    Partners page added

---
+++ drupal/sites/default/files/php/twig/6636f431ed327_page--front.html.twig_TEGA1g6dl7sGKgBSw8HUIdxM5/P08FcvYaCj7TFVqaQ7ekYLUaKRSZb_vB_W7pNLEySHA.php
@@ -0,0 +1,148 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/cadetech_theme/templates/system/page--front.html.twig */
+class __TwigTemplate_810497f80ab84255165181ae3916a3cc extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"above-header\">
+    ";
+        // line 2
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "above_header", [], "any", false, false, true, 2), 2, $this->source), "html", null, true);
+        echo "
+</div>
+<header class=\"header\">
+    <div class=\"top-header\">
+        ";
+        // line 6
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "branding", [], "any", false, false, true, 6), 6, $this->source), "html", null, true);
+        echo "
+        ";
+        // line 7
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "navigation", [], "any", false, false, true, 7), 7, $this->source), "html", null, true);
+        echo "
+        ";
+        // line 8
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "search", [], "any", false, false, true, 8), 8, $this->source), "html", null, true);
+        echo "
+    </div>
+    <section class=\"featured\">
+        ";
+        // line 11
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "featured", [], "any", false, false, true, 11), 11, $this->source), "html", null, true);
+        echo "
+    </section>
+    <div class=\"front-header\">
+        ";
+        // line 14
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "front_header", [], "any", false, false, true, 14), 14, $this->source), "html", null, true);
+        echo "
+    </div>
+    <div class=\"bottom-header\">
+        ";
+        // line 17
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "bottom_header", [], "any", false, false, true, 17), 17, $this->source), "html", null, true);
+        echo "
+    </div>
+</header>
+<section class=\"main\">
+   <main class=\"content\">
+     ";
+        // line 22
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "content", [], "any", false, false, true, 22), 22, $this->source), "html", null, true);
+        echo "
+   </main>
+   <aside class=\"right-sidebar\">
+     ";
+        // line 25
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "right_sidebar", [], "any", false, false, true, 25), 25, $this->source), "html", null, true);
+        echo "
+   </aside>
+</section>
+<footer class=\"footer\">
+    ";
+        // line 29
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "footer", [], "any", false, false, true, 29), 29, $this->source), "html", null, true);
+        echo "
+</footer>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/cadetech_theme/templates/system/page--front.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  96 => 29,  89 => 25,  83 => 22,  75 => 17,  69 => 14,  63 => 11,  57 => 8,  53 => 7,  49 => 6,  42 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/cadetech_theme/templates/system/page--front.html.twig", "/opt/drupal/web/themes/custom/cadetech_theme/templates/system/page--front.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 2);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
